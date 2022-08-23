2b079355b0f043adb249726dd68b6a847db2db1b
Newton Ombese <newtonombese1@gmail.com>
2022-08-23 10:39:18 +0300

    Completed Assignments

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+    attr_accessor :page_count, :title, :author, :genre
+    def initialize (title)
+        @title = title
+    end
+    
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
-
+first_book = Book.new("Introduction to Ruby")
+first_book.author = "Newton Ombese"
+first_book.genre = "Learn Ruby with Me"
+first_book.page_count = 695
+first_book.turn_page
